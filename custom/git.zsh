116d117a57696e0f00ec66e9f92e38b593057c66
ych <ych@cs.nctu.edu.tw>
2012-11-07 15:38:36 +0800

    Alias some commands to avoid glob problem of zsh

--- custom/git.zsh
+++ custom/git.zsh
@@ -1,1 +1,4 @@
+alias git='noglob git'
+alias gdump='git cat-file -p'
+alias gtype='git cat-file -t'
 alias glog='git log --pretty=format:"%h %ad | %s%d" --graph --date=iso --since="7 days ago" --author="Yu-Chun Huang" --all'
