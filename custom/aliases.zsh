a6f0f8dedce2acc8449a1fba2d182ea2a53e6bc2
YC Huang <yc.huang@latticeworkinc.com>
2021-12-07 14:00:09 +0800

    add aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,1 +1,5 @@
 alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+alias vip='f() { VBoxManage guestproperty get $1 "/VirtualBox/GuestInfo/Net/0/V4/IP" };f'
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias grep=rg
