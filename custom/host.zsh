ac5aa324560e4f233dbf8a05083ade893f216704
ych <ych@ychuang.org>
2022-11-30 13:26:09 +0800

    change host setting arch

--- custom/host.zsh
+++ custom/host.zsh
@@ -1,5 +1,11 @@
-if [ -d $ZSH_CUSTOM/host/`hostname` ]; then
-	for config_file ($ZSH_CUSTOM/host/`hostname`/*.zsh(N)); do
-		source $config_file
-	done
+if [ -d $ZSH_CUSTOM/host/`hostname -d` ]; then
+    if [ -d $ZSH_CUSTOM/host/`hostname -d`/`hostname` ]; then
+        for config_file ($ZSH_CUSTOM/host/`hostname -d`/`hostname`/*.zsh); do
+            source $config_file
+        done
+    else
+        for config_file ($ZSH_CUSTOM/host/`hostname -d`/*.zsh); do
+            source $config_file
+        done
+    fi
 fi
